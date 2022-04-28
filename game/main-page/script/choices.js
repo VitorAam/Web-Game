8cbbdabcde3d338cebaa0d6bed014af2e4a52c4c
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-04-28 18:47:30 -0300

    att_js
    Implementei opção de cancelar para o texto poder ser relido antes de finalizar a escolha de caminho.

--- game/main-page/script/choices.js
+++ game/main-page/script/choices.js
@@ -1,6 +1,7 @@
 function startGame() {
     var selected = prompt('Escolha uma jornada')
-    while (selected != '1' && selected != '2' && selected != '3'){
+    console.log(selected)
+    while (selected != '1' && selected != '2' && selected != '3' && selected != null){
         window.alert('Escolha inválida, selecione entre 1 e 3!')
         selected = prompt('Tente novamente: Lembre, entre 1 e 3!')
     }
@@ -15,7 +16,7 @@ function startGame() {
 
 var escolha = function pathSelected() {
     var path = prompt('Qual caminho você vai seguir?')
-    while (path != '1' && path != '2') {
+    while (path != '1' && path != '2' && path != null) {
         window.alert('Escolha inválida, selecione entre o caminho 1 ou o caminho 2!')
         path = prompt('Tente novamente: Lembre, entre 1 e 2!')
     }
